dc4df52806bb38d25b30dc55be5a302516446ae8
Jody Septiawan <jody@192.168.1.2>
2021-07-04 22:06:52 +0700

    state class component

---
+++ src/ExampleForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { 
+        Container, 
+        Row, Col, Form, 
+        Button } from 'react-bootstrap'
+
+class ExampleForm extends Component {
+  constructor(props){
+    super(props)
+    // init state attribute here
+  }
+
+  handleOnChange = (e) => {
+    // setState here
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault()
+    //print state value with console.log here
+  }
+
+  render(){
+    return (
+      <Container>
+        <Row 
+          className="d-flex align-items-center justify-content-center vh-100">
+          <Col md="6">
+          <Form onSubmit={this.handleOnSubmit}>
+            <div className="text-center h3">Register</div>
+            <Form.Group className="mb-3" controlId="formFullName">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control 
+                onChange={this.handleOnChange} 
+                value={this.state.fullname}
+                name="fullname" size="sm" type="text" 
+                placeholder="Enter Full Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control 
+                onChange={this.handleOnChange} 
+                value={this.state.email}
+                name="email" size="sm" type="email" 
+                placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                onChange={this.handleOnChange} 
+                value={this.state.password}
+                name="password" size="sm" type="password" 
+                placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" size="sm">
+              Submit
+            </Button>
+          </Form>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default ExampleForm;
